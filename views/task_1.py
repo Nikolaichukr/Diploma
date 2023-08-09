f20d14c119161b61711f979f081c2a2cc621cbb1
Roman Nikolaichuk <51542378+Nikolaichukr@users.noreply.github.com>
2023-08-09 05:07:41 +0300

    rewritten front-end

--- views/task_1.py
+++ views/task_1.py
@@ -7,11 +7,15 @@ task1 = Blueprint('task1', __name__)
 @task1.route("/", methods=["GET", "POST"])
 def task1_page():
     if request.method == "POST":
-        amount = int(request.form["amount"])
-        start_range = int(request.form["start_range"])
-        end_range = int(request.form["end_range"])
+        jobs_amount_min = int(request.form["jobs_amount_min"])
+        jobs_amount_max = int(request.form["jobs_amount_max"])
+        jobs_duration_min = int(request.form["jobs_duration_min"])
+        jobs_duration_max = int(request.form["jobs_duration_max"])
+        tests_amount = int(request.form["tests_amount"])
+        test_name = int(request.form["test_name"])
 
-        xml_content = generate_quiz_xml(amount, start_range, end_range)
+        # xml_content = generate_quiz_xml(amount, start_range, end_range)
+        xml_content = generate_quiz_xml(5, 1, 15)
 
         # Create a response with appropriate headers to force file download
         response = make_response(xml_content)
